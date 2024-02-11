14fa1e1dfe43a387b1afd34d10e9eafc38789ee9
Qizhe Lao <qlao@ucscl.com>
2024-02-11 03:48:17 -0800

    Font and Load

---
+++ src/scenes/Load.js
@@ -0,0 +1,43 @@
+class Load extends Phaser.Scene {
+    constructor() {
+        super('loadScene')
+    }
+
+    preload() {
+        // loading bar
+        // see: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/loader/
+        let loadingBar = this.add.graphics()
+        this.load.on('progress', (value) => {
+            loadingBar.clear()                              // reset fill/line style
+            loadingBar.fillStyle(0xFFFFFF, 1)               // (color, alpha)
+            loadingBar.fillRect(0, centerY, w * value, 5)   // (x, y, w, h)
+        })
+        this.load.on('complete', () => {
+            loadingBar.destroy()
+        })
+
+        this.load.path = './assets/'
+        // load graphics assets
+        this.load.image('Background', 'img/Background.png')
+        this.load.image('NormalPlatform', 'img/NormalPlatform.png')
+        this.load.image('Player', 'img/Player.png')
+        this.load.image('SpikeDown', 'img/SpikeDown.png')
+        this.load.image('SpikeUp', 'img/SpikeUp.png')
+        this.load.image('Wall', 'img/Wall.png')
+        
+        // load audio assets
+        // this.load.audio('beats', ['audio/beats.mp3'])
+        // this.load.audio('clang', ['audio/clang.mp3'])
+        // this.load.audio('death', ['audio/death.mp3'])
+        // load font
+        this.load.bitmapFont('gem', 'font/gem.png', 'font/gem.xml')
+    }
+
+    create() {
+        // check for local storage browser support
+        window.localStorage ? console.log('Local storage supported') : console.log('Local storage not supported')
+
+        // go to Title scene
+        this.scene.start('titleScene')
+    }
+}
